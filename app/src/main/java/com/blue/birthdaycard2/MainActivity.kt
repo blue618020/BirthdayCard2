e5c5b9e9e6dddcdddb366bec518a166e22760a0e
blue618020 <blue618020@gmail.com>
2024-01-30 22:56:38 +0900

    240130_텍스트와 레이아웃 요소추가

--- app/src/main/java/com/blue/birthdaycard2/MainActivity.kt
+++ app/src/main/java/com/blue/birthdaycard2/MainActivity.kt
@@ -3,13 +3,21 @@ package com.blue.birthdaycard2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.blue.birthdaycard2.ui.theme.BirthdayCard2Theme
 
@@ -23,6 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    GreetingText(message = "Happy Birthday Card!", from = "From Enna")
                 }
             }
         }
@@ -38,12 +47,25 @@ class MainActivity : ComponentActivity() {
 //}
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier){
-    Text(
-        text = message,
-        fontSize = 30.sp,
-        lineHeight = 100.sp,
-    )
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ){
+        Text(
+            text = message,
+            fontSize = 30.sp,
+            lineHeight = 100.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 15.sp,
+            modifier = modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
 @Preview(
@@ -52,6 +74,7 @@ fun GreetingText(message: String, modifier: Modifier = Modifier){
 @Composable
 fun BirthdayCardPreview() {
     BirthdayCard2Theme {
-        GreetingText(message = "Happy Birthday blue!")
+        GreetingText(message = "Happy Birthday blue!",
+                    from = "From Enna")
     }
 }
